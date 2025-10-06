45c2a5d434de390c842afeb91391bf64971d1f21
lyfm8 <lndk2k5@gmail.com>
2025-10-06 10:10:31 +0700

    sua bfs

--- algorithm_ff.py
+++ algorithm_ff.py
@@ -26,52 +26,7 @@ class algorithm:
         self.ui=ui
 
 
-    # ---------- DFS ----------
-    def dfs_solver(self, grid, colors, idx):
-        # Kiểm tra stop request
-        if self.ui.stop_requested:
-            return False, None
-
-        if idx == len(colors):
-            return True, grid
-        color = colors[idx]
-        start, end = self.ui.pairs[color]
-        #self.ui.log(f"➡️ Đang xử lý màu {color.upper()} từ {start} đến {end}")
-
-        def backtrack(path, visited):
-            # Kiểm tra stop request
-            if self.ui.stop_requested:
-                return False, None
-
-
-            r, c = path[-1]
-            if (r, c) == end:
-                new_grid = [row[:] for row in grid]
-                for (pr, pc) in path:
-                    new_grid[pr][pc] = color
-                self.ui.log(f"✅ Tìm thấy đường cho màu {color}")
-                ok, res = self.dfs_solver(new_grid, colors, idx+1)
-                if ok:
-                    # Vẽ đường hoàn chỉnh cho màu này
-                    self.ui.paint_path(path, colors[idx])
-                    return True, res
-
-                return False, None
-
-            for dr, dc in [(0,1),(0,-1),(1,0),(-1,0)]:
-                nr, nc = r+dr, c+dc
-                if 0 <= nr < self.ui.grid_size and 0 <= nc < self.ui.grid_size:
-                    if (nr, nc) not in visited and (grid[nr][nc] == '' or (nr, nc) == end):
-                        visited.add((nr, nc))
-                        path.append((nr, nc))
-                        ok, res = backtrack(path, visited)
-                        if ok: return True, res
-                        path.pop()
-                        visited.remove((nr, nc))
-
-            return False, None
-
-        return backtrack([start], {start})
+    
 
     # ---------- BFS ----------
     def bfs_solver(self, grid, colors):
@@ -88,7 +43,7 @@ class algorithm:
             path = self.bfs_find_path(grid, start, end, color)
             if not path:
                 self.ui.log(f"⚠️ Không tìm được đường cho màu {color}")
-                continue
+                return False, None
             if path:
                 new_grid = [row[:] for row in grid]
                 for (r, c) in path:
@@ -135,3 +90,7 @@ class algorithm:
                         parents[(nr, nc)] = (r, c)
                         q.append((nr, nc))
         return None
+    
+
+    #----------IDS----------
+    
